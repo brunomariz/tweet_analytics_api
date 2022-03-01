748b806e8887eda2366ea205c665c981ef41d950
Bruno Mariz <bruno.mariz@semantix.inc>
2022-02-28 23:54:20 -0300

    :drill: add error handling

--- src/twitter_handler/user_id.py
+++ src/twitter_handler/user_id.py
@@ -1,4 +1,5 @@
 import requests
+from fastapi import HTTPException
 from credentials.api import BEARER_TOKEN
 
 def get_user_data_by_username(username):
@@ -6,5 +7,7 @@ def get_user_data_by_username(username):
     url = f'https://api.twitter.com/2/users/by/username/{username}?user.fields=created_at'
 
     r = requests.get(url, headers=headers)
+    if ('errors' in r.json().keys()):
+        raise HTTPException(400, r.json()['errors'])
     data = r.json()['data']
     return {'id': data['id'], 'name': data['name'], 'username': data['username'], 'createdAt': data['created_at']}
